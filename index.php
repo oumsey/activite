1e241126185fc4ee1f5b6ea16274646b808ae84c
oumsey <=>
2020-01-20 12:31:30 +0000

    modification du fichier index.php avec ajout des champs de formulaire

--- index.php
+++ index.php
@@ -10,7 +10,44 @@
 <body>
 
 	<form action="" method="post" enctype="multipart/form-data">
+		<label for="nom">Nom:</label>
+		<input type="text" name="nom" id="nom" maxlength="10" required>
 
+			<br>
+
+		<label for="prenom">Prenom:</label>
+		<input type="text" name="prenom" id="prenom" maxlength="20" required>
+			
+			<br><br>
+
+		<label for="personne">Type de personne:</label>
+		<select name="personne" id="personne">
+			<option value="default">Choisir un type</option>
+			
+		</select>
+
+			<br><br><br>
+
+		<label>Sexe:</label>
+		<span id="sexe">
+			<span>H</span><input type="radio" name="sexe" value="H">
+			<span>F</span><input type="radio" name="sexe" value="F">
+		</span>
+		
+			<br>
+
+		<input type="file" name="photos" id="photos" title="Ajoutez une photo">
+			
+			<br>
+
+		<label for="datenaissance">Date de naissance:</label>
+		<input type="date" name="datenaissance" id="datenaissance" required>
+			
+			<br>
+
+		<input type="submit" name="envoie" value="S'enroler" id="valide">
+
+		<?php if (isset($message)) { ?> <p><?= $message ?></p>  <?php } ?>
 	</form>
 
 
