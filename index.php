245e8c7a838b436066d626a582bf65f1c324ef22
oumsey <=>
2020-01-20 12:03:45 +0000

    Ajout du fichier index.php au repository

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Enrolement</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="styles.css">
+	<link rel="shortcut icon" type="image/x-icon" href="image/logo.png">
+</head>
+<body>
+
+	<form action="" method="post" enctype="multipart/form-data">
+
+	</form>
+
+
+</body>
+</html>
